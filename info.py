062f857e563764c826cf70a3a4d3a62c8e947c11
Botmaster55 <wajsarif461@gmail.com>
2026-05-08 23:33:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,11 +30,11 @@ DB_NAME = environ.get("DATABASE_NAME", "testing0")
 # =========================================================
 ADMINS = int(environ.get("ADMINS", "1249672673"))
 
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002133932311"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002580860502"))
 PREMIUM_LOGS = int(environ.get("PREMIUM_LOGS", "-1003184409377"))
 VERIFIED_LOG = int(environ.get("VERIFIED_LOG", "-1003184409377"))
 
-POST_CHANNEL = int(environ.get("POST_CHANNEL", "-1002314687215"))
+POST_CHANNEL = int(environ.get("POST_CHANNEL", "-1002580860502"))
 VIDEO_CHANNEL = int(environ.get("VIDEO_CHANNEL", "-1002623534183"))
 BRAZZER_CHANNEL = int(environ.get("BRAZZER_CHANNEL", "-1002259803190"))
 
